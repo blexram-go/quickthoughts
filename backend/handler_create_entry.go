5cbd3977c7650bfbc8d691a21f895e319f2f728f
brianalexramos <brian_ramos_14@yahoo.com>
2025-02-18 13:28:17 -0800

    Add a create entry handler. Created json helper functions to assist with sending json and errors

---
+++ backend/handler_create_entry.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/blexram-go/quickthoughts/backend/internal/database"
+	"github.com/google/uuid"
+)
+
+type Entry struct {
+	ID         uuid.UUID `json:"id"`
+	Created_At time.Time `json:"create_at"`
+	Updated_At time.Time `json:"updated_at"`
+	Title      string    `json:"title"`
+	Body       string    `json:"body"`
+}
+
+func (cfg *apiConfig) handlerCreateEntry(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Title string `json:"title"`
+		Body  string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
+		return
+	}
+
+	validatedTitle, err := validateTitleLength(params.Title)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err.Error())
+	}
+
+	validatedBody, err := validateBodyLength(params.Body)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	entry, err := cfg.db.CreateEntry(context.Background(), database.CreateEntryParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Title:     validatedTitle,
+		Body:      validatedBody,
+	})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create entry")
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, Entry{
+		ID:         entry.ID,
+		Created_At: entry.CreatedAt,
+		Updated_At: entry.UpdatedAt,
+		Title:      entry.Title,
+		Body:       entry.Body,
+	})
+
+}
+
+func validateTitleLength(title string) (string, error) {
+	const maxTitleLength = 140
+	if len(title) > maxTitleLength {
+		return "", errors.New("Entry title exceeds max length")
+	}
+	return title, nil
+}
+
+func validateBodyLength(body string) (string, error) {
+	const maxBodyLength = 4096
+	if len(body) > maxBodyLength {
+		return "", errors.New("Entry body exceeds max length")
+	}
+	return body, nil
+}
--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+
+	"github.com/blexram-go/quickthoughts/backend/internal/database"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	db *database.Queries
+}
 
 func main() {
 	port := "8080"
-	// db_url := "postgres://brianramos:@localhost:5432/quickthoughts?sslmode=disable"
+	dbURL := "postgres://brianramos:@localhost:5432/quickthoughts?sslmode=disable"
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("error connecting to database: %v", err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+
+	cfg := &apiConfig{
+		db: dbQueries,
+	}
 
 	mux := http.NewServeMux()
+
+	mux.Handle("/", http.FileServer(http.Dir("./frontend/build")))
+	mux.HandleFunc("/create", cfg.handlerCreateEntry)
 
 	server := http.Server{
 		Addr:    ":" + port,
